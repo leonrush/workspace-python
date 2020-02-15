635eaaf7142ab6404ea70f9fc57966de10598010
leonrush <wqiang023@139.com>
2020-02-15 22:05:33 +0800

    更改bug，增加测试日志
    1. defined "%virtualenv_path%"  更改为defined virtualenv_path
    2. 优化提示
    3. 增加测试日志

--- env-tool/create_virenv.bat
+++ env-tool/create_virenv.bat
@@ -1,21 +1,21 @@
-﻿@REM @Author: wangqiang
+@REM @Author: wangqiang
 @REM @Date:   2020-02-15 10:23:51
-@REM @Last Modified by:   leonrush
-@REM Modified time: 2020-02-15 17:55:43
+@REM @Last Modified by:   wqiang
+@REM Modified time: 2020-02-15 21:43:09
 
 @echo off
 rem 文件设置为utf8,同时代码中显示切成utf-8代码页，保证文件在传输和显示过程一致，不会出现乱码
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-echo ****************step1:虚拟环境设置**********************
+echo ****************step1:虚拟环境设置**********************************
 
 rem set "project_path=%~dp0"
 echo 当前目录:%cd%
 echo.
 :_set_virtualenv_path
-set /p virtualenv_path=请设置虚拟环境目录:
-if not defined "%virtualenv_path%" (set virtualenv_path=%cd%)
+set /p virtualenv_path=请设置虚拟环境目录(默认为当前目录):
+if not defined virtualenv_path (set virtualenv_path=%cd%)
 if exist "%virtualenv_path%" (
     echo.virtualenv_path=%virtualenv_path%
     ) else (
@@ -30,14 +30,14 @@ for /f %%i in (.\python_path.txt) do (
     echo %%i
     %%i --version)
 
-set /p python_path=<"python_path.txt"
 echo.
 :_set_local_path
-set local_python_path=
-set /p local_python_path=请设置本地python路径：
-if not defined "%local_python_path%" (set /p local_python_path=<"python_path.txt")
-if exist "%local_python_path%" (
-    echo.local_python_path:%local_python_path%
+set local_python_file=
+set /p local_python_file=请设置本地python(默认为第一个)：
+if not defined local_python_file (set /p local_python_file=<"python_path.txt" )
+
+if exist "%local_python_file%" (
+    echo.local_python_file:%local_python_file%
     ) else (
     echo 错误：文件不存在，请重新设置！
     goto _set_local_path )
@@ -49,11 +49,11 @@ set /p virtualenv_name=请设置虚拟环境名称：
 if defined virtualenv_name (
     echo.virtualenv_name=%virtualenv_name%
     ) else (
-    echo 名称不能为空，请重新设置！
+    echo 错误：名称不能为空，请重新设置！
     goto _set_virtualenv_name )
 
 
-echo ****************step2:构建虚拟环境%virtualenv_name%***********************
+echo ****************step2:构建虚拟环境**********************************
 set activate_path="%virtualenv_path%/%virtualenv_name%/Scripts/activate"
 if exist %activate_path% (
     echo 虚拟环境已存在，python版本：
@@ -64,10 +64,10 @@ if exist %activate_path% (
     )
 
 cd /d "%virtualenv_path%"
-pip install virtualenv && virtualenv  --no-site-packages -p %local_python_path% %virtualenv_name% && echo 虚拟环境已创建.
+pip install virtualenv && virtualenv  --no-site-packages -p %local_python_file% %virtualenv_name% && echo 虚拟环境已创建.
 
 :_is_test
-echo ****************step3:测试虚拟环境****************************
+echo ****************step3:测试虚拟环境**********************************
 set /p is_test=是否测试虚拟环境? [Y/N]:
 if /i '%is_test%'=='Y' goto _test_virtual
 if /i '%is_test%'=='N' exit
